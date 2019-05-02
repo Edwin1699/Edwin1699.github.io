6cb8b75efa54f412a48e3cfe8e88e44e5f9b837f
Edwin1699 <47002298+Edwin1699@users.noreply.github.com>
2019-05-02 13:08:47 -0400

    Add files via upload

---
+++ wd final/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+  $(".gundamsmall").mouseenter(function(){
+    $(".gundam").addClass("show");
+  });
+  $(".gundamsmall").mouseleave(function(){
+    $(".gundam").removeClass("show");
+  });
+
+  $(".gundamzsmall").mouseenter(function(){
+    $(".gundamzeta").addClass("show");
+  });
+  $(".gundamzsmall").mouseleave(function(){
+    $(".gundamzeta").removeClass("show");
+  });
+
+  $(".gundamzzsmall").mouseenter(function(){
+    $(".gundamzzeta").addClass("show");
+  });
+  $(".gundamzzsmall").mouseleave(function(){
+    $(".gundamzzeta").removeClass("show");
+  });
+
+    $(".nugundamsmall").mouseenter(function(){
+    $(".nugundam").addClass("show");
+  });
+  $(".nugundamsmall").mouseleave(function(){
+    $(".nugundam").removeClass("show");
+  });
+
+  $(".gundamf91small").mouseenter(function(){
+    $(".gundamf91").addClass("show");
+  });
+  $(".gundamf91small").mouseleave(function(){
+    $(".gundamf91").removeClass("show");
+  });
+
+    $(".ggundamsmall").mouseenter(function(){
+    $(".ggundam").addClass("show");
+  });
+  $(".ggundamsmall").mouseleave(function(){
+    $(".ggundam").removeClass("show");
+  });
+
+ $(".gundamwingsmall").mouseenter(function(){
+    $(".gundamwing").addClass("show");
+  });
+  $(".gundamwingsmall").mouseleave(function(){
+    $(".gundamwing").removeClass("show");
+  });
+
+ $(".turnagundamsmall").mouseenter(function(){
+    $(".turnagundam").addClass("show");
+  });
+  $(".turnagundamsmall").mouseleave(function(){
+    $(".turnagundam").removeClass("show");
+  });
+
+ $(".seeddestinygundamsmall").mouseenter(function(){
+    $(".seeddestinygundam").addClass("show");
+  });
+  $(".seeddestinygundamsmall").mouseleave(function(){
+    $(".seeddestinygundam").removeClass("show");
+  });
+
+  $(".gundam00small").mouseenter(function(){
+    $(".gundam00").addClass("show");
+  });
+  $(".gundam00small").mouseleave(function(){
+    $(".gundam00").removeClass("show");
+  });
+
+  $(".unicorngundamsmall").mouseenter(function(){
+    $(".unicorngundam").addClass("show");
+  });
+  $(".unicorngundamsmall").mouseleave(function(){
+    $(".unicorngundam").removeClass("show");
+  });
+
+   $(".gundambarbatossmall").mouseenter(function(){
+    $(".gundambarbatos").addClass("show");
+  });
+  $(".gundambarbatossmall").mouseleave(function(){
+    $(".gundambarbatos").removeClass("show");
+  });
+
+
+});
