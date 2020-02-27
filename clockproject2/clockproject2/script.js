1591745223bb7b43df2e2da6aa0b2afb20ccbb9e
Edwin1699 <47002298+Edwin1699@users.noreply.github.com>
2020-02-27 13:55:18 -0500

    Add files via upload

---
+++ clockproject2/clockproject2/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".meteor1").addClass("alarm");
+    $(".meteor2").addClass("alarm");
+    $(".meteor3").addClass("alarm");
+    $(".meteor4").addClass("alarm");
+    $(".meteor5").addClass("alarm");
+    $(".meteor6").addClass("alarm");
+    $(".meteor7").addClass("alarm");
+    $(".button").addClass("alarm");
+  }, 3000);
+});
